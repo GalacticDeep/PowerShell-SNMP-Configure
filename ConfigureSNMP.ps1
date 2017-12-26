d38d7077db16c16a6b48131fd4cd3c109dce1be1
Ben <benjamin.stitt@gmail.com>
2017-12-26 16:04:57 -0500

    Draft
    Rough draft started

---
+++ ConfigureSNMP.ps1
@@ -0,0 +1,27 @@
+﻿#start and configure initial SNMP setup for new environment
+# WORK IN PROGRESS
+#
+# Set traps under community names: "public & GCT#%*"
+#localhost is OM's IP address
+# must accept SNMP packets from any host
+# finally restart the SNMP host
+
+
+#variables
+$servermanager = @("enter from Notepad for multiple servers")
+$community = @("public & GCT#%*")
+
+#import server manager
+Import-Module ServerManager
+
+#Check If SNMP Services Are Already Installed
+$check = Get-WindowsFeature | Where-Object {$_.Name -eq "SNMP-Services"}
+If ($check.Installed -ne "True") {
+	#Install/Enable SNMP Services
+	Add-WindowsFeature SNMP-Services | Out-Null
+}
+
+#Start SNMP
+Start-Service SNMP $($servermanager)
+
+
