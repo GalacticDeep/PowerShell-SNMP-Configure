16b36126f9a66d55d754ba9991b30cc08d3f5201
Ben <benjamin.stitt@gmail.com>
2017-12-28 16:43:44 -0500

    Final draft for testing
    Tested every command individually on my own PC, but will test whole script on test servers this weekend.

--- ConfigureSNMP.ps1
+++ ConfigureSNMP.ps1
@@ -1,15 +1,21 @@
 ﻿#start and configure initial SNMP setup for new environment
-# WORK IN PROGRESS
 #
+#NOT FOR PRODUCTION USE
+#Currently in testing
+#
+# Select all agent services
 # Set traps under community names: "public & GCT#%*"
-#localhost is OM's IP address
+# localhost is OM's IP address
 # must accept SNMP packets from any host
 # finally restart the SNMP host
 
+#use Invoke-Command -ComputerName pc1, pc2 -FilePath "script path"
+
 
 #variables
-$servermanager = @("enter from Notepad for multiple servers")
-$community = @("public & GCT#%*")
+$path = null
+$OMaddress = @("internal IP address of OM")
+
 
 #import server manager
 Import-Module ServerManager
@@ -21,7 +27,32 @@ If ($check.Installed -ne "True") {
 	Add-WindowsFeature SNMP-Services | Out-Null
 }
 
-#Start SNMP
-Start-Service SNMP $($servermanager)
+#Select all agent services for SNMP
+$path = 'HKLM:\SYSTEM\CurrentControlSet\Services\SNMP\Parameters\RFC1156Agent\' 
+Set-ItemProperty -Path $path -Name sysServices  -Value 79
+
+#Set traps to public and GCT#%*
+
+$path = 'HKCU:\SYSTEM\CurrentControlSet\Services\SNMP\Parameters\TrapConfiguration\public'
+if(!(Test-Path -Path $path))
+{
+    New-Item -Path $path
+}
+
+$path = 'HKCU:\SYSTEM\CurrentControlSet\Services\SNMP\Parameters\TrapConfiguration\GCT#%*'
+if(!(Test-Path -Path $path))
+{
+    New-Item -Path $path
+}
+
+#Set GCT#%* trap destination to localhost and to OM's IP address
+Set-ItemProperty -Path $path -Name 1  -Value localhost
+Set-ItemProperty -Path $path -Name 2  -Value $OMaddress
+
+#Set to accept packets from any host
+Remove-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\SNMP\Parameters\PermittedManagers' -Name *
+
+#Restart SNMP
+Restart-Service SNMP $($servermanager)
 
 
